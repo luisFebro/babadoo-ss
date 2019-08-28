b6129c95861dbaea116cecb7c642b00082ab7cd3
Luis Febro <mr.febro@gmail.com>
2019-08-28 18:20:13 -0400

    Updates

---
+++ precache-manifest.96de071dc50a367819ab8e1f8fee4213.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3aac26def896f530f1f0b12354aef2d",
+    "url": "/ecommerce_babadoo-pt/index.html"
+  },
+  {
+    "revision": "22564987d2435f783c41",
+    "url": "/ecommerce_babadoo-pt/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "cab739f203912c0a02ab",
+    "url": "/ecommerce_babadoo-pt/static/css/main.8c90372d.chunk.css"
+  },
+  {
+    "revision": "22564987d2435f783c41",
+    "url": "/ecommerce_babadoo-pt/static/js/2.88976a44.chunk.js"
+  },
+  {
+    "revision": "cab739f203912c0a02ab",
+    "url": "/ecommerce_babadoo-pt/static/js/main.6601aa97.chunk.js"
+  },
+  {
+    "revision": "d78b4919d2177cded9d3",
+    "url": "/ecommerce_babadoo-pt/static/js/runtime~main.79700e99.js"
+  },
+  {
+    "revision": "28b21f3237501369f859635dae39a463",
+    "url": "/ecommerce_babadoo-pt/static/media/Organica 90.28b21f32.woff"
+  }
+]);
