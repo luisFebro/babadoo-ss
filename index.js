f1273a9b7e79069464d9c447d7feaffe0f87c9e0
Luis Febro <mr.febro@gmail.com>
2019-09-10 06:14:22 -0400

    resctructing to full-stack - backend, added UnderConstruction modal, new payment

---
+++ index.js
@@ -0,0 +1,73 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const nodemailer = require("nodemailer");
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.post('/api/form', (req, res) => {
+    const htmlEmail = `
+        <center>
+            <header">
+                 <img style="box-shadow: 0 19px 38px rgba(0,0,0,0.20), 0 15px 12px rgba(0,0,0,0.12);" src="https://imgur.com/9GjtAiW.png" width="200px" height="200px"/>
+            </header>
+        </center>
+        <h1>A Babadoo tem um novo pedido!</h1>
+        <h3>Descrição a seguir:</h3>
+        <ul>
+          <li>Nome Cliente: ${req.body.name}</li>
+          <li>Contato/Whatsapp: ${req.body.phone}</li>
+          <li>Endereço para Entrega: ${req.body.address}</li>
+          <li>Informações Adicionais: ${req.body.additional}</li>
+        </ul>
+        <h3>Encomenda:</h3>
+        <ul>
+          <li>Quantidades e Items(ref): <br/>${req.body.itemDescription}</li>
+          <br/>
+          <li>Valor Total: ${req.body.totalPay}</li>
+        </ul>
+        <footer>
+            <strong>Relatório de compra gerado pelo site Babadoo web</strong>
+        </footer>
+    `;
+
+    let transporter = nodemailer.createTransport({
+        host: 'smtp.gmail.com',
+        port: 587,
+        secure: false, // true for 465, false for other ports
+        auth: {
+            user: 'babadooweb@gmail.com', // generated ethereal user
+            pass: 'babadoo2019' // generated ethereal password
+        },
+        tls: {
+            rejectUnauthorized: false
+        }
+    });
+
+    let mailOptions = {
+        from: '"Babadoo loja web - novo pedido" Babadoo Web', // sender address
+        to: 'babadooweb@gmail.com', // 'babadoo.sexyshop.lingeries@gmail.com' list of receivers babadoo.sexyshop.lingeries@gmail.com
+        subject: 'Babadoo loja web - novo pedido de compra', // Subject line
+        text: 'no-message-sent-ignore-here', // plain text body
+        html: htmlEmail // html body
+    };
+
+    // send mail with defined transport object
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            return console.log(error);
+        }
+        console.log('Message sent: %s', info.messageId);
+        console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+
+        //res.render('contact', {msg:'PEDIDO ENVIADO! ENTRAREMOS EM CONTATO O MAIS BREVE POSSÍVEL'});
+    });
+
+});
+
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+});
