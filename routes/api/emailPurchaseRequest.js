41ecfd6dd9ef693af4d10fb0fee9f53b1cdd1493
Luis Febro <mr.febro@gmail.com>
2019-10-07 09:50:54 -0400

    stage 3 - new snackbars from material-ui - updates5

--- routes/api/emailPurchaseRequest.js
+++ routes/api/emailPurchaseRequest.js
@@ -37,8 +37,8 @@ router.post('/', (req, res) => {
         port: 587,
         secure: false, // true for 465, false for other ports
         auth: {
-            user: "babadooweb@gmail.com", // generated ethereal user
-            pass: "babadoo2019" // generated ethereal password
+            user: nodemailerEmail, // generated ethereal user
+            pass: nodemailerPassword // generated ethereal password
         },
         tls: {
             rejectUnauthorized: false
