4e4ef0e7119dc9f5b92bccf3a05384e8123e7598
Luis Febro <mr.febro@gmail.com>
2019-11-18 01:24:17 -0400

    fullstack - updated MVC for admin update

--- client/src/redux/actions/adminActions.js
+++ client/src/redux/actions/adminActions.js
@@ -5,7 +5,7 @@ import { configTypeJson } from '../../utils/server/configTypeJson';
 import { getBodyRequest } from '../../utils/server/getBodyRequest';
 // naming structure: action > type > speficification e.g action: GET_MODAL_BLUE / func: getModalBlue
 
-// UPDATED DATA
+
 export const readAdmin = async dispatch => {
     try {
         // setLoadingOn(dispatch);
